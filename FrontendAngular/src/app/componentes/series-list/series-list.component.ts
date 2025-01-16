594b38be6324d8871be86ec267087dad8d5f4bb7
Emilio <eljishiemilio@gmail.com>
2025-01-16 18:23:16 +0100

    avances y creacion de funciones, creacion de nuevas categorias para el modal

--- FrontendAngular/src/app/componentes/series-list/series-list.component.ts
+++ FrontendAngular/src/app/componentes/series-list/series-list.component.ts
@@ -3,6 +3,8 @@ import {SerieService} from "../../services/serie.service";
 import {Serie} from "../../common/serie";
 import {MatIconModule} from "@angular/material/icon";
 import { MatButtonModule } from '@angular/material/button';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+import {SeriesModalComponent} from "../series-modal/series-modal.component";
 
 
 @Component({
@@ -18,6 +20,7 @@ import { MatButtonModule } from '@angular/material/button';
 
 export class SeriesListComponent {
 private readonly  serieService: SerieService=inject(SerieService);
+private readonly  modalService: NgbModal=inject(NgbModal);
 
 series:Serie[] = [];
 categorias:string[]=[];
@@ -50,4 +53,36 @@ constructor() {
   }
 
 
+  eliminarSerie(serie: Serie) {
+    if (confirm("Estas seguro de eliminar la serie?")){
+      this.serieService.deleteSerie(serie._id).subscribe(
+        {
+          next:value => {
+            console.log(value)
+          },
+          error: err => {
+            console.error(err.message)
+          }
+        }
+      )
+    }
+  }
+
+  anadirSerie() {
+    const modalRef= this.modalService.open(SeriesModalComponent)
+    modalRef.componentInstance.editar=false;
+    modalRef.componentInstance.categorias=this.categorias;
+    modalRef.result.then(()=>{this.loadSeries()})
+      .catch(err=>{this.loadSeries()})
+
+  }
+
+  editarSerie(serie: Serie) {
+    const modalRef= this.modalService.open(SeriesModalComponent)
+    modalRef.componentInstance.serie=serie;
+    modalRef.componentInstance.editar=true;
+    modalRef.componentInstance.categorias=this.categorias;
+    modalRef.result.then(()=>{this.loadSeries()})
+      .catch(err=>{this.loadSeries()})
+  }
 }
--- FrontendAngular/src/app/componentes/series-modal/series-modal.component.ts
+++ FrontendAngular/src/app/componentes/series-modal/series-modal.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject, Input, OnInit} from '@angular/core';
+import {Serie} from "../../common/serie";
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {SerieService} from "../../services/serie.service";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-series-modal',
@@ -7,6 +11,70 @@ import { Component } from '@angular/core';
   templateUrl: './series-modal.component.html',
   styleUrl: './series-modal.component.css'
 })
-export class SeriesModalComponent {
+export class SeriesModalComponent implements OnInit {
 
+  @Input() serie!: Serie;
+  @Input({required:true})editar!: boolean;
+  @Input({required:true})categoria!: string;
+
+  activeModal:NgbActiveModal=inject(NgbActiveModal);
+  private readonly seriesService: SerieService=inject(SerieService);
+  private readonly formBuilder: FormBuilder=inject(FormBuilder);
+
+
+  formSeries:FormGroup=this.formBuilder.group({
+    _id:[''],
+    titulo:[''],
+    categorias:[],
+    imagenes:[''],
+    capitulos:[''],
+    emision:[''],
+    sinopsis:[''],
+
+  });
+
+  anadirCategoria:FormGroup=this.formBuilder.group({
+    nuevaCategoria: [''],
+  })
+
+  get titulo():any{
+    return this.formSeries.get('titulo');
+  }
+  get categorias():any{
+    return this.formSeries.get('categorias');
+  }
+  get capitulos():any{
+    return this.formSeries.get('capitulos');
+  }
+  get emision():any{
+    return this.formSeries.get('emision');
+  }get sinopsis():any{
+    return this.formSeries.get('sinopsis');
+  }
+  get nuevaCategoria():any{
+  return this.anadirCategoria.get('nuevaCategoria');
+}
+
+
+
+  ngOnInit() {
+    if (this.editar) {
+      this.formSeries.setValue(this.serie);
+    }else {
+      this.formSeries.reset();
+    }
+  }
+
+  anadirNuevaCategoria(nuevaCategoria:any){
+    if(!this.editar)this.categorias.push(nuevaCategoria);
+    else {
+      let nuevaCategoria;
+      nuevaCategoria = this.formSeries.getRawValue().categoria;
+      nuevaCategoria.push(nuevaCategoria);
+      this.formSeries.setControl("categorias", new FormControl(nuevaCategoria));
+      this.categorias.push(nuevaCategoria)
+
+    }
+    this.anadirCategoria.reset();
+  }
 }
